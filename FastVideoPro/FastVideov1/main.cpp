20499d8e1c9a8ba4649912fa7f5ff67f21a05ad9
mk-z <zyf079@gmail.com>
2016-05-13 18:14:05 +0800

    same FastVideo debug run error

--- FastVideoPro/FastVideov1/main.cpp
+++ FastVideoPro/FastVideov1/main.cpp
@@ -187,6 +187,28 @@ void checkDirectory(){
     }
 }
 
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+	QByteArray localMsg = msg.toLocal8Bit();
+	switch (type) {
+	case QtDebugMsg:
+		fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		break;
+		/*case QtInfoMsg:
+		fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		break;*/
+	case QtWarningMsg:
+		fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		break;
+	case QtCriticalMsg:
+		fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		break;
+	case QtFatalMsg:
+		fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+		abort();
+	}
+}
+
 int main(int argc, char *argv[])
 {
     
@@ -194,7 +216,11 @@ int main(int argc, char *argv[])
 	SetUnhandledExceptionFilter(MyUnhandledExceptionFilter);
 
 	SingleApplication a(argc, argv);
-    qInstallMessageHandler(outputMessage);
+#ifndef _DEBUG
+	qInstallMessageHandler(outputMessage);
+#else
+	qInstallMessageHandler(myMessageOutput);
+#endif
 	if (a.isRunning())
 	{
 		return 0;
